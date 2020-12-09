d139d88748a9234d53407bcf47c0bfd493424a84
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2020-12-09 11:15:50 -0800

    Read all available bytes from ClientSession.

--- src/main.c
+++ src/main.c
@@ -140,10 +140,12 @@ send_request_and_read_response(int sockfd, void *client_session,
       }
       fprintf(stderr, "read %d bytes from socket\n", n);
 
-      // Now pull those bytes from the buffer into ClientSession.
-      // Note that we pass buf, n; not buf, sizeof(buf). We don't
-      // want to pull in unitialized memory that we didn't just
-      // read from the socket.
+      /*
+       * Now pull those bytes from the buffer into ClientSession.
+       * Note that we pass buf, n; not buf, sizeof(buf). We don't
+       * want to pull in unitialized memory that we didn't just
+       * read from the socket.
+       */
       n = rustls_client_session_read_tls(client_session, buf, n);
       if(n == 0) {
         fprintf(stderr, "EOF from ClientSession::read_tls\n");
@@ -155,23 +157,30 @@ send_request_and_read_response(int sockfd, void *client_session,
         return 1;
       }
 
-      result = rustls_client_session_process_new_packets(client_session);
+      int result = rustls_client_session_process_new_packets(client_session);
       if(result != CRUSTLS_OK) {
         fprintf(stderr, "Error in process_new_packets");
         return 1;
       }
 
-      memset(buf, 0, sizeof(buf));
-      n = rustls_client_session_read(client_session, buf, sizeof(buf));
-      if(n == 0) {
-        fprintf(stderr, "EOF from ClientSession::read (this is expected)\n");
-      }
-      else if(n < 0) {
-        fprintf(stderr, "Error in ClientSession::read\n");
-        return 1;
-      }
+      /* Read all available bytes from the client_session until EOF.
+       * Note that EOF here indicates "no more bytes until
+       * process_new_packets", not "stream is closed".
+       */
+      for(;;) {
+        memset(buf, 0, sizeof(buf));
+        n = rustls_client_session_read(client_session, buf, sizeof(buf));
+        if(n == 0) {
+          fprintf(stderr, "EOF from ClientSession::read (this is expected)\n");
+          break;
+        }
+        else if(n < 0) {
+          fprintf(stderr, "Error in ClientSession::read\n");
+          return 1;
+        }
 
-      write_all(STDOUT_FILENO, buf, n);
+        write_all(STDOUT_FILENO, buf, n);
+      }
     }
     if(rustls_client_session_wants_write(client_session) &&
        (events[0].events & EPOLLOUT) > 0) {
