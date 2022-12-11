a289250be385c70110592fc62886c1fac03fc8d7
Jacob Hoffman-Andrews <github@hoffman-andrews.com>
2022-12-10 22:03:54 -0800

    Update rustls from 0.20.4 to 0.20.7 (#279)
    And use new BorrowedBuf/BorrowedCursor API.

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::io::Write;
 use std::{env, fs, path::PathBuf};
 
 // Keep in sync with Cargo.toml.
-const RUSTLS_CRATE_VERSION: &str = "0.20.4";
+const RUSTLS_CRATE_VERSION: &str = "0.20.7";
 
 fn main() {
     let out_dir = PathBuf::from(env::var_os("OUT_DIR").unwrap());
