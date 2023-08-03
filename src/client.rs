ff8bbfded71b9523687fa05d86264cb7f485676a
Daniel McCarney <daniel@binaryparadox.net>
2023-08-03 13:54:58 -0400

    error: track upstream removal of cert SCT support.
    This commit removes the error handling related to certificate SCTs. The
    upstream Rustls project removed embedded SCT support in 0.22.x.

--- src/client.rs
+++ src/client.rs
@@ -76,7 +76,6 @@ impl ServerCertVerifier for NoneVerifier {
         _end_entity: &Certificate,
         _intermediates: &[Certificate],
         _server_name: &rustls::ServerName,
-        _scts: &mut dyn Iterator<Item = &[u8]>,
         _ocsp_response: &[u8],
         _now: SystemTime,
     ) -> Result<ServerCertVerified, rustls::Error> {
@@ -231,7 +230,6 @@ impl rustls::client::ServerCertVerifier for Verifier {
         end_entity: &Certificate,
         intermediates: &[Certificate],
         server_name: &rustls::ServerName,
-        _scts: &mut dyn Iterator<Item = &[u8]>,
         ocsp_response: &[u8],
         _now: SystemTime,
     ) -> Result<ServerCertVerified, rustls::Error> {
