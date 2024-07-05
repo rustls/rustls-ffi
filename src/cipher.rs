824382e6f38b665995787aac425fce215d156e67
Daniel McCarney <daniel@binaryparadox.net>
2024-07-05 16:15:46 -0400

    cipher: remove hardcoded ring ciphersuites
    The provider model replaces these.

--- src/cipher.rs
+++ src/cipher.rs
@@ -10,7 +10,6 @@ use std::sync::Arc;
 use pki_types::{CertificateDer, CertificateRevocationListDer};
 use rustls::client::danger::ServerCertVerifier;
 use rustls::client::WebPkiServerVerifier;
-use rustls::crypto::ring::{ALL_CIPHER_SUITES, DEFAULT_CIPHER_SUITES};
 use rustls::server::danger::ClientCertVerifier;
 use rustls::server::WebPkiClientVerifier;
 use rustls::sign::CertifiedKey;
@@ -98,112 +97,6 @@ pub extern "C" fn rustls_supported_ciphersuite_get_name(
         Err(_) => rustls_str::from_str_unchecked(""),
     }
 }
-
-/// Return the length of rustls' list of supported cipher suites.
-#[no_mangle]
-pub extern "C" fn rustls_all_ciphersuites_len() -> usize {
-    ALL_CIPHER_SUITES.len()
-}
-
-/// Get a pointer to a member of rustls' list of supported cipher suites.
-///
-/// This will return non-NULL for i < rustls_all_ciphersuites_len().
-///
-/// The returned pointer is valid for the lifetime of the program and may
-/// be used directly when building a ClientConfig or ServerConfig.
-#[no_mangle]
-pub extern "C" fn rustls_all_ciphersuites_get_entry(
-    i: size_t,
-) -> *const rustls_supported_ciphersuite {
-    match ALL_CIPHER_SUITES.get(i) {
-        Some(cs) => cs as *const SupportedCipherSuite as *const _,
-        None => null(),
-    }
-}
-
-/// Return the length of rustls' list of default cipher suites.
-#[no_mangle]
-pub extern "C" fn rustls_default_ciphersuites_len() -> usize {
-    DEFAULT_CIPHER_SUITES.len()
-}
-
-/// Get a pointer to a member of rustls' list of supported cipher suites.
-///
-/// This will return non-NULL for i < rustls_default_ciphersuites_len().
-///
-/// The returned pointer is valid for the lifetime of the program and may
-/// be used directly when building a ClientConfig or ServerConfig.
-#[no_mangle]
-pub extern "C" fn rustls_default_ciphersuites_get_entry(
-    i: size_t,
-) -> *const rustls_supported_ciphersuite {
-    match DEFAULT_CIPHER_SUITES.get(i) {
-        Some(cs) => cs as *const SupportedCipherSuite as *const _,
-        None => null(),
-    }
-}
-
-/// Rustls' list of supported cipher suites.
-///
-/// This is an array of pointers, and its length is given by
-/// `RUSTLS_ALL_CIPHER_SUITES_LEN`. The pointers will always be valid.
-/// The contents and order of this array may change between releases.
-#[no_mangle]
-pub static mut RUSTLS_ALL_CIPHER_SUITES: [*const rustls_supported_ciphersuite; 9] = [
-    &rustls::crypto::ring::cipher_suite::TLS13_AES_256_GCM_SHA384 as *const SupportedCipherSuite
-        as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS13_AES_128_GCM_SHA256 as *const SupportedCipherSuite
-        as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
-        as *const SupportedCipherSuite as *const _,
-];
-
-/// The length of the array `RUSTLS_ALL_CIPHER_SUITES`.
-#[no_mangle]
-pub static RUSTLS_ALL_CIPHER_SUITES_LEN: usize = unsafe { RUSTLS_ALL_CIPHER_SUITES.len() };
-
-/// Rustls' list of default cipher suites.
-///
-/// This is an array of pointers, and its length is given by
-/// `RUSTLS_DEFAULT_CIPHER_SUITES_LEN`. The pointers will always be valid.
-/// The contents and order of this array may change between releases.
-#[no_mangle]
-pub static mut RUSTLS_DEFAULT_CIPHER_SUITES: [*const rustls_supported_ciphersuite; 9] = [
-    &rustls::crypto::ring::cipher_suite::TLS13_AES_256_GCM_SHA384 as *const SupportedCipherSuite
-        as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS13_AES_128_GCM_SHA256 as *const SupportedCipherSuite
-        as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS13_CHACHA20_POLY1305_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256
-        as *const SupportedCipherSuite as *const _,
-    &rustls::crypto::ring::cipher_suite::TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305_SHA256
-        as *const SupportedCipherSuite as *const _,
-];
-
-/// The length of the array `RUSTLS_DEFAULT_CIPHER_SUITES`.
-#[no_mangle]
-pub static RUSTLS_DEFAULT_CIPHER_SUITES_LEN: usize = unsafe { RUSTLS_DEFAULT_CIPHER_SUITES.len() };
 
 arc_castable! {
     /// The complete chain of certificates to send during a TLS handshake,
@@ -1116,49 +1009,21 @@ impl rustls_server_cert_verifier {
 #[cfg(test)]
 mod tests {
     use super::*;
-    use std::str;
+
+    use crate::crypto_provider::{
+        rustls_default_crypto_provider_ciphersuites_get,
+        rustls_default_crypto_provider_ciphersuites_len,
+    };
 
     #[test]
-    fn all_cipher_suites_arrays() {
-        assert_eq!(RUSTLS_ALL_CIPHER_SUITES_LEN, ALL_CIPHER_SUITES.len());
-        for (original, ffi) in ALL_CIPHER_SUITES
-            .iter()
-            .zip(unsafe { RUSTLS_ALL_CIPHER_SUITES }.iter().copied())
-        {
-            let ffi_cipher_suite = try_ref_from_ptr!(ffi);
-            assert_eq!(original, ffi_cipher_suite);
+    fn default_cipher_suites() {
+        let num_suites = rustls_default_crypto_provider_ciphersuites_len();
+        assert!(num_suites > 2);
+        for i in 0..num_suites {
+            let suite = rustls_default_crypto_provider_ciphersuites_get(i);
+            let name = rustls_supported_ciphersuite_get_name(suite);
+            let name = unsafe { name.to_str() };
+            println!("{}: {}", i, name);
         }
-    }
-
-    #[test]
-    fn default_cipher_suites_arrays() {
-        assert_eq!(
-            RUSTLS_DEFAULT_CIPHER_SUITES_LEN,
-            DEFAULT_CIPHER_SUITES.len()
-        );
-        for (original, ffi) in DEFAULT_CIPHER_SUITES
-            .iter()
-            .zip(unsafe { RUSTLS_DEFAULT_CIPHER_SUITES }.iter().copied())
-        {
-            let ffi_cipher_suite = try_ref_from_ptr!(ffi);
-            assert_eq!(original, ffi_cipher_suite);
-        }
-    }
-
-    #[test]
-    fn ciphersuite_get_name() {
-        let suite = rustls_all_ciphersuites_get_entry(0);
-        let s = rustls_supported_ciphersuite_get_name(suite);
-        let want = "TLS13_AES_256_GCM_SHA384";
-        unsafe {
-            let got = str::from_utf8(slice::from_raw_parts(s.data as *const u8, s.len)).unwrap();
-            assert_eq!(want, got)
-        }
-    }
-
-    #[test]
-    fn test_all_ciphersuites_len() {
-        let len = rustls_all_ciphersuites_len();
-        assert!(len > 2);
     }
 }
