8e12458910745d673f398d9715ea45472db329dd
Daniel McCarney <daniel@binaryparadox.net>
2024-11-25 14:19:30 -0500

    cmake: add connect-test and integration-test targets
    This ports the `.PHONY` targets for running a platform verifier
    connection test and the integration tests. Both are ignored by default
    during normal `cargo test` invocations because they depend on the
    client/server binaries being built (and may make network requests in the
    case of the connect-test target).
    
    Run these like usual on a UNIX system:
    ```
    cmake -S . -B build
    cmake --build build --target connect-test
    cmake --build build --target integration-test
    ```

--- cmake/rust.cmake
+++ cmake/rust.cmake
@@ -28,3 +28,31 @@ add_custom_target(
     COMMAND cbindgen > "src/rustls.h"
     WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
 )
+
+add_custom_target(
+    connect-test
+    DEPENDS client
+    COMMAND
+        ${CMAKE_COMMAND} -E env RUSTLS_PLATFORM_VERIFIER=1
+        ${CMAKE_BINARY_DIR}/tests/client example.com 443 /
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+if(WIN32)
+    set(CLIENT_BINARY "${CMAKE_BINARY_DIR}\\tests\\$<CONFIG>\\client.exe")
+    set(SERVER_BINARY "${CMAKE_BINARY_DIR}\\tests\\$<CONFIG>\\server.exe")
+else()
+    set(CLIENT_BINARY "${CMAKE_BINARY_DIR}/tests/client")
+    set(SERVER_BINARY "${CMAKE_BINARY_DIR}/tests/server")
+endif()
+
+add_custom_target(
+    integration-test
+    DEPENDS client server
+    COMMAND
+        ${CMAKE_COMMAND} -E env CLIENT_BINARY=${CLIENT_BINARY} ${CMAKE_COMMAND}
+        -E env SERVER_BINARY=${SERVER_BINARY} cargo test --locked
+        ${CARGO_FEATURES} "$<IF:$<CONFIG:Release>,--release,>" --test
+        client_server client_server_integration -- --ignored --exact
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
