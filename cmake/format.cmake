ce06f5bc13ba82315fac525079c5e0218688f4fd
Daniel McCarney <daniel@binaryparadox.net>
2024-11-25 14:07:41 -0500

    cmake: targets for checking/fixing code fmt
    We have several `.PHONY` targets in the GNU `Makefile` that handle code
    formatting. This commit extends the CMake build system to be able to do
    the same. We add `-fix` and `-check` targets for:
    
    * `rust-format-fix` and `rust-format-check` for running/checking `cargo
      fmt` for rust code.
    * `cmake-format-fix` and `cmake-format-check` for running/checking
      Gersemi for CMake files.
    * `c-format-fix` and `c-format-check` for running/checking
      `clang-format` on the test .c/.h files.
    * `format-fix` and `format-check` for handling all of the above.
    
    You can use these from a UNIX system like:
    
    ```
    cmake -B build -S .
    cmake --build build --target format-fix
    
    cmake --build build --target help
    ```

---
+++ cmake/format.cmake
@@ -0,0 +1,67 @@
+add_custom_target(
+    rust-format-fix
+    COMMAND sed -i -e 's/ffi_panic_boundary! {/if true {/g' src/*.rs
+    COMMAND cargo fmt
+    COMMAND sed -i -e 's/if true {/ffi_panic_boundary! {/g' src/*.rs
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+add_custom_target(
+    rust-format-check
+    COMMAND sed -i -e 's/ffi_panic_boundary! {/if true {/g' src/*.rs
+    COMMAND cargo fmt --check
+    COMMAND sed -i -e 's/if true {/ffi_panic_boundary! {/g' src/*.rs
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+add_custom_target(
+    cmake-format-fix
+    COMMAND
+        gersemi --no-warn-about-unknown-commands -i CMakeLists.txt
+        tests/CMakeLists.txt cmake/*.cmake
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+add_custom_target(
+    cmake-format-check
+    COMMAND
+        gersemi --no-warn-about-unknown-commands -c CMakeLists.txt
+        tests/CMakeLists.txt cmake/*.cmake
+    WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}"
+)
+
+function(add_c_format_targets targets)
+    set(all_sources "")
+    foreach(target ${targets})
+        list(APPEND all_sources $<TARGET_PROPERTY:${target},SOURCES>)
+    endforeach()
+
+    # A fix target that formats the source files in-place.
+    add_custom_target(
+        c-format-fix
+        COMMAND clang-format -i ${all_sources}
+        COMMAND_EXPAND_LISTS
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/tests"
+    )
+
+    # A check target that checks if the source files are formatted correctly.
+    add_custom_target(
+        c-format-check
+        COMMAND clang-format --dry-run -Werror -i ${all_sources}
+        COMMAND_EXPAND_LISTS
+        WORKING_DIRECTORY "${CMAKE_CURRENT_SOURCE_DIR}/tests"
+    )
+endfunction()
+
+# Add c-format-fix and c-format-check targets for the test binary sourcecode.
+add_c_format_targets("client;server")
+
+add_custom_target(
+    format-fix
+    DEPENDS rust-format-fix c-format-fix cmake-format-fix
+)
+
+add_custom_target(
+    format-check
+    DEPENDS rust-format-check c-format-check cmake-format-check
+)
