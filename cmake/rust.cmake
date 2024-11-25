355a64a50074dfe86659f587370f2350b3c3718b
Daniel McCarney <daniel@binaryparadox.net>
2024-11-25 16:49:13 -0500

    cmake: use cargo capi to build lib
    This is _much_ easier, provides a real `install` command, and can
    provide dynamic linking support that integrates well with `pkg-config`.

--- cmake/rust.cmake
+++ cmake/rust.cmake
@@ -5,14 +5,15 @@ ExternalProject_Add(
     rustls-ffi
     DOWNLOAD_COMMAND ""
     CONFIGURE_COMMAND ""
-    BUILD_COMMAND ""
-    COMMAND
-        cargo build --locked ${CARGO_FEATURES}
+    BUILD_COMMAND
+        cargo capi build --locked ${CARGO_FEATURES}
         "$<IF:$<CONFIG:Release>,--release,-->"
     # Rely on cargo checking timestamps, rather than tell CMake where every
     # output is.
     BUILD_ALWAYS true
-    INSTALL_COMMAND ""
+    INSTALL_COMMAND
+        cargo capi install --libdir=lib --prefix=${CMAKE_BINARY_DIR}/rust
+        --locked ${CARGO_FEATURES} "$<IF:$<CONFIG:Release>,--release,--debug>"
     # Run cargo test with --quiet because msbuild will treat the presence
     # of "error" in stdout as an error, and we have some test functions that
     # end in "_error". Quiet mode suppresses test names, so this is a
