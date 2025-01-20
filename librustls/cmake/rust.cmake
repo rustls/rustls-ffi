ff725163cf0508c829d091055205b2b1ab833a84
Daniel McCarney <daniel@binaryparadox.net>
2025-01-20 15:18:04 -0500

    rm CI dep on rustls-ffi-test for artifact test
    Previously the artifacts.yaml CI job relied on an external repo that had
    a simple C/CMake application depending on librustls. When verifying
    artifacts we cloned this repo and tested a build against the artifact.
    
    This commit reworks the main repo's test application code/build to
    support this use-case. The main tweak required is to support _not_
    building librustls from src, but instead finding it with pkg-config. We
    gate this behind a new `FORCE_SYSTEM_RUSTLS` option. The artifact test
    CI is updated to use this option.
    
    Annoyingly we have to patch the .pc prefix to an absolute dir to get
    this working nicely, which means handling Windows specially.
    
    Similarly, for yet to be determined reasons, on Win32 we have to add
    some extra target_link_libraries that don't propagate automatically
    through the pc config. For this we just shift the existing Win32
    specific target_link_libraries invocation up and use it for both
    pkg-config librustls and build-it-ourselves librustls.

--- librustls/cmake/rust.cmake
+++ librustls/cmake/rust.cmake
@@ -1,27 +1,41 @@
 include(ExternalProject)
 set_directory_properties(PROPERTIES EP_PREFIX ${CMAKE_BINARY_DIR}/rust)
 
-ExternalProject_Add(
-    rustls-ffi
-    DOWNLOAD_COMMAND ""
-    CONFIGURE_COMMAND ""
-    BUILD_COMMAND
-        cargo capi build --locked ${CARGO_FEATURES}
-        "$<IF:$<CONFIG:Release>,--release,-->"
-    # Rely on cargo checking timestamps, rather than tell CMake where every
-    # output is.
-    BUILD_ALWAYS true
-    INSTALL_COMMAND
-        cargo capi install --libdir=lib --prefix=${CMAKE_BINARY_DIR}/rust
-        --locked ${CARGO_FEATURES} "$<IF:$<CONFIG:Release>,--release,--debug>"
-    # Run cargo test with --quiet because msbuild will treat the presence
-    # of "error" in stdout as an error, and we have some test functions that
-    # end in "_error". Quiet mode suppresses test names, so this is a
-    # sufficient workaround.
-    TEST_COMMAND
-        cargo test --locked ${CARGO_FEATURES}
-        "$<IF:$<CONFIG:Release>,--release,-->" --quiet
-)
+if(FORCE_SYSTEM_RUSTLS)
+    find_package(PkgConfig REQUIRED)
+    pkg_check_modules(RUSTLS_FFI REQUIRED rustls)
+
+    if(NOT RUSTLS_FFI_FOUND)
+        message(FATAL_ERROR "System rustls-ffi required but not found")
+    endif()
+
+    message(STATUS "RUSTLS_FFI_INCLUDE_DIRS: ${RUSTLS_FFI_INCLUDE_DIRS}")
+    message(STATUS "RUSTLS_FFI_LIBRARY_DIRS: ${RUSTLS_FFI_LIBRARY_DIRS}")
+    message(STATUS "RUSTLS_FFI_LIBRARIES: ${RUSTLS_FFI_LIBRARIES}")
+else()
+    ExternalProject_Add(
+        rustls-ffi
+        DOWNLOAD_COMMAND ""
+        CONFIGURE_COMMAND ""
+        BUILD_COMMAND
+            cargo capi build --locked ${CARGO_FEATURES}
+            "$<IF:$<CONFIG:Release>,--release,-->"
+        # Rely on cargo checking timestamps, rather than tell CMake where every
+        # output is.
+        BUILD_ALWAYS true
+        INSTALL_COMMAND
+            cargo capi install --libdir=lib --prefix=${CMAKE_BINARY_DIR}/rust
+            --locked ${CARGO_FEATURES}
+            "$<IF:$<CONFIG:Release>,--release,--debug>"
+        # Run cargo test with --quiet because msbuild will treat the presence
+        # of "error" in stdout as an error, and we have some test functions that
+        # end in "_error". Quiet mode suppresses test names, so this is a
+        # sufficient workaround.
+        TEST_COMMAND
+            cargo test --locked ${CARGO_FEATURES}
+            "$<IF:$<CONFIG:Release>,--release,-->" --quiet
+    )
+endif()
 
 add_custom_target(
     cbindgen
